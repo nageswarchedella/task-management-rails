1690228821062594206759fdb1ac15aa7dc08acf
Nageswar Chedella <nageswarchedella@gmail.com>
2024-02-05 22:52:08 +0530

    added project table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_05_171413) do
+ActiveRecord::Schema.define(version: 2024_02_05_171935) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
