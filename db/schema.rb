2e1ab96f16829ef6624fa30c8c35bb4540e3e65f
Nageswar Chedella <nageswarchedella@gmail.com>
2024-09-21 21:51:10 +0530

    rails upgrade to 7.2
    Signed-off-by: Nageswar Chedella <nageswarchedella@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_05_172842) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_02_05_172842) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sprints", force: :cascade do |t|
@@ -24,15 +23,15 @@ ActiveRecord::Schema.define(version: 2024_02_05_172842) do
     t.date "start_date"
     t.date "end_date"
     t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,8 +39,8 @@ ActiveRecord::Schema.define(version: 2024_02_05_172842) do
     t.string "email"
     t.string "username"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
